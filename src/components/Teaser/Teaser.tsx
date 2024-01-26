0a430c14e54d30d1941dcd5b4f3f438d4a0f1d7f
Nico Poggenburg <n.poggenburg@leuchtfeuer.com>
2024-01-26 23:13:47 +0100

    unintentional change

--- src/components/Teaser/Teaser.tsx
+++ src/components/Teaser/Teaser.tsx
@@ -19,7 +19,7 @@ const Teaser: FC<TeaserProps> = (args: TeaserProps) => {
       </a>
       <div className="p-5 flex-grow">
         <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <h5 className="mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             {args.title}
           </h5>
         </a>
